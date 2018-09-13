e129abf0c419b4ccba1a63223f7258a59f65120a
炊军辉 <jack_chui@talkmate.com>
2018-09-13 20:36:46 +0800

    课程进度调整

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import user from './modules/user'
 import course from './modules/course'
 import learnSystem from './modules/learnSystem'
-
+import 'babel-polyfill'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
