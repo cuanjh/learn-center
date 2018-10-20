0c09efc8c6008ec64e07542c78b7f9cce3d0ecc1
炊军辉 <jack_chui@talkmate.com>
2018-10-20 18:55:00 +0800

    浏览器控制

--- src/components/learnSystem/learn.vue
+++ src/components/learnSystem/learn.vue
@@ -40,6 +40,14 @@ Vue.component('form-makesentence', makeSentence)
 Vue.component('form-fillgap', fillGap)
 
 export default {
+  beforeCreate () {
+    let browser = this.getOSAndBrowser().browser
+    let reg = new RegExp('\\d+', 'g')
+    let version = browser.match(reg)[0]
+    if (!((browser.indexOf('Firefox') > -1 && version >= 21) || (browser.indexOf('Chrome') > -1 && version >= 17) || (browser.indexOf('Opera') > -1 && version >= 18))) {
+      location.href = 'update.html'
+    }
+  },
   created () {
     Bus.$on('initAnonymousData', () => {
       this.initAnonymousData()
@@ -87,16 +95,90 @@ export default {
 
       await this.getRecord(this.currentChapterCode + '-A0')
       await this.getProgress(this.currentChapterCode)
+    },
+    getOSAndBrowser () {
+      let h = navigator.platform
+      let e = navigator.userAgent
+      let g = []
+      let f = {os: 'Other', browser: 'Other'}
+      if (h.indexOf('Win') > -1) {
+        if (e.indexOf('Windows NT 5.0') > -1) {
+          f.os = 'Windows 2000'
+        } else {
+          if (e.indexOf('Windows NT 5.1') > -1) {
+            f.os = 'Windows XP'
+          } else {
+            if (e.indexOf('Windows NT 5.2') > -1) {
+              f.os = 'Windows 2003'
+            } else {
+              if (e.indexOf('Windows NT 6.0') > -1) {
+                f.os = 'Windows Vista'
+              } else {
+                if (e.indexOf('Windows NT 6.1') > -1 || e.indexOf('Windows 7') > -1) {
+                  f.os = 'Windows 7'
+                } else {
+                  if (e.indexOf('Windows 8') > -1) {
+                    f.os = 'Windows 8'
+                  } else {
+                    if (e.indexOf('Windows NT 10.0') > -1) {
+                      f.os = 'Windows 10'
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      } else {
+        if (h.indexOf('Mac') > -1) {
+          f.os = 'Mac'
+        } else {
+          if (h.indexOf('X11') > -1) {
+            f.os = 'Unix'
+          } else {
+            if (h.indexOf('Linux') > -1) {
+              f.os = 'Linux'
+            }
+          }
+        }
+      }
+      if (/[Ff]irefox(\/\d+\.\d+)/.test(e)) {
+        g = /([Ff]irefox)\/(\d+\.\d+)/.exec(e)
+      } else {
+        if (/MSIE \d+\.\d+/.test(e)) {
+          g = /MS(IE) (\d+\.\d+)/.exec(e)
+        } else {
+          if (/[Cc]hrome\/\d+/.test(e)) {
+            g = /([Cc]hrome)\/(\d+)/.exec(e)
+          } else {
+            if (/[Vv]ersion\/\d+\.\d+\.\d+(\.\d)* *[Ss]afari/.test(e)) {
+              g = /[Vv]ersion\/(\d+\.\d+\.\d+)(\.\d)* *([Ss]afari)/.exec(e)
+            } else {
+              if (/[Oo]pera.+[Vv]ersion\/\d+\.\d+/.test(e)) {
+                g = /([Oo]pera).+[Vv]ersion\/(\d+)\.\d+/.exec(e)
+              } else {
+                if (/rv:11.0\) like Gecko/.test(e)) {
+                  g = ['IEEdge', 'IE', '11']
+                }
+              }
+            }
+          }
+        }
+      }
+      if (g.length >= 3) {
+        f.browser = g[1] + g[2]
+      }
+      return f
     }
   }
 }
 </script>
 
 <style lang="less">
-  // @import '../../../static/less/lib.less';
-  @import '../../../static/less/ui-dialog.less';
-  @import '../../../static/less/animate.less';
-  @import "../../../static/less/toast.less";
-  @import "../../../static/less/learnsystem.less";
-  @import "../../../static/less/index.less";
+// @import '../../../static/less/lib.less';
+@import "../../../static/less/ui-dialog.less";
+@import "../../../static/less/animate.less";
+@import "../../../static/less/toast.less";
+@import "../../../static/less/learnsystem.less";
+@import "../../../static/less/index.less";
 </style>
