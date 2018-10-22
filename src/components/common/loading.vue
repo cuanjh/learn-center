d3f4946bd9964b534f07c13723ad0a0a4367cbfb
常宏芳 <hongfang_chang@talkmate.com>
2018-10-22 19:35:23 +0800

    loading

--- src/components/common/loading.vue
+++ src/components/common/loading.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="loading">
-    <div class="spinner">
-      <div class="dot1"></div>
-      <div class="dot2"></div>
+    <div class="loader">
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="describe">正在加载</div>
     </div>
   </div>
 </template>
@@ -19,41 +23,105 @@
   display: flex;
   align-items: center;
   justify-content: center;
+  -webkit-transition: all .3s ease-in-out;
+  -moz-transition: all .3s ease-in-out;
+  -ms-transition: all .3s ease-in-out;
+  -o-transition: all .3s ease-in-out;
+  transition: all .3s ease-in-out;
 }
-
-.spinner {
-  margin: 100px auto;
-  width: 40px;
-  height: 40px;
-  position: relative;
-  text-align: center;
-  animation: sk-rotate 2.0s infinite linear;
-}
-
-.dot1, .dot2 {
-  width: 60%;
-  height: 60%;
-  display: inline-block;
+.loader {
   position: absolute;
-  top: 0;
-  background-color: #2097DD;
+  top: 50%;
+  left: 50%;
+  margin-left: 10%;
+  transform: translate3d(-50%, -50%, 0);
+  -webkit-transition: all .3s ease-in-out;
+  -moz-transition: all .3s ease-in-out;
+  -ms-transition: all .3s ease-in-out;
+  -o-transition: all .3s ease-in-out;
+  transition: all .3s ease-in-out;
+}
+.describe {
+  font-size: 16px;
+  color: #64aacc;
+      text-align: center;
+    margin-top: 20px;
+}
+.dot {
+  width: 24px;
+  height: 24px;
+  background: #3ac;
   border-radius: 100%;
-  animation: sk-bounce 2.0s infinite ease-in-out;
+  margin-left: 6px;
+  display: inline-block;
+  animation: slide 1s infinite;
 }
-
-.dot2 {
-  top: auto;
-  bottom: 0;
-  animation-delay: -1.0s;
+.dot:nth-child(1) {
+  animation-delay: 0.1s;
+  background: #0e99c0;
 }
-
-@keyframes sk-rotate { 100% { transform: rotate(360deg); }}
-
-@keyframes sk-bounce {
-  0%, 100% {
-    transform: scale(0.0);
-  } 50% {
-    transform: scale(1.0);
+.dot:nth-child(2) {
+  animation-delay: 0.2s;
+  background: #387696;
+}
+.dot:nth-child(3) {
+  animation-delay: 0.3s;
+  background: #7e96c0;
+}
+.dot:nth-child(4) {
+  animation-delay: 0.4s;
+  background: #c396c9;
+}
+.dot:nth-child(5) {
+  animation-delay: 0.5s;
+  background: #fa94c0;
+}
+@-moz-keyframes slide {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    opacity: 0.6;
+    transform: scale(1.3);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@-webkit-keyframes slide {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    opacity: 0.6;
+    transform: scale(1.3);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@-o-keyframes slide {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    opacity: 0.6;
+    transform: scale(1.3);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes slide {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    opacity: 0.6;
+    transform: scale(1.3);
+  }
+  100% {
+    transform: scale(1);
   }
 }
 </style>
