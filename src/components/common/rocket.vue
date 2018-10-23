04243c4f2593598bcabbb2b6c1c961a3870f1a4e
常宏芳 <hongfang_chang@talkmate.com>
2018-10-23 14:00:49 +0800

    update

--- src/components/common/rocket.vue
+++ src/components/common/rocket.vue
@@ -9,7 +9,6 @@ import $ from 'jquery'
 export default {
   data () {
     return {
-      backTopBtn: false,
       rocketShow: false
     }
   },
@@ -18,23 +17,11 @@ export default {
     window.onscroll = function () {
       // scrollTop=滚动条相对于其顶部的偏移。
       let _headerDistance = $(window).scrollTop()
-      // bottomScroll=页面的总高度 - 当前窗口的高度
-      let _bottomScroll = $(document).height() - $(window).height()
-
-      // 返回顶部的功能,不过好像已经没有使用了
-      if (_headerDistance >= 100) {
-        _this.backTopBtn = true
-      } else {
-        _this.backTopBtn = false
-      }
       // 是否显示小火箭的功能
       if (_headerDistance >= window.innerHeight) {
         _this.rocketShow = true
-      }
-      if (_headerDistance >= 59 && _bottomScroll > 120) {
-        _this.learnfixed = true
-      } else if (_headerDistance < 59) {
-        _this.learnfixed = false
+      } else {
+        _this.rocketShow = false
       }
     }
   },
