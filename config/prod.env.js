511b04998778d23f51095f646f57f96508d05f5b
炊军辉 <jack_chui@talkmate.com>
2019-03-06 20:07:51 +0800

    test

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,11 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_HOST: '"http://api.talkmate.com"',
+  API_HOST: '"http://dev.api.talkmate.com"',
   LOGIN_URL: '"http://test-learn.talkmate.com/app/index"',
   REDIRECT_URL: '"http://test-learn.talkmate.com/app/pay-success"',
   // LOGIN_URL: '"http://beat-study.talkmate.com/app/index"',
   APP_KEY: '"talkmateVersion"',
-  APP_SECRET: '"5EAE76C6323C85D2E35D4817020D84C9"' // 线上
-  // APP_SECRET: '"E6DAC3DC3514681FC922ECE1B0CF06EB"' // 测试
+  // APP_SECRET: '"5EAE76C6323C85D2E35D4817020D84C9"' // 线上
+  APP_SECRET: '"E6DAC3DC3514681FC922ECE1B0CF06EB"' // 测试
 }
