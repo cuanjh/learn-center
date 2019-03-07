e16939517736f639831ef362b74a5809c8743565
炊军辉 <jack_chui@talkmate.com>
2019-03-07 11:11:45 +0800

    测试环境配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,9 +5,9 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_HOST: '"https://dev.api.talkmate.com"',
-  LOGIN_URL: '"https://test-learn.talkmate.com/app/index"',
-  REDIRECT_URL: '"https://test-learn.talkmate.com/app/pay-success"',
+  API_HOST: '"http://dev.api.talkmate.com"',
+  LOGIN_URL: '"http://test-learn.talkmate.com/app/index"',
+  REDIRECT_URL: '"http://test-learn.talkmate.com/app/pay-success"',
   APP_KEY: '"talkmateVersion"',
   APP_SECRET: '"E6DAC3DC3514681FC922ECE1B0CF06EB"' // 测试
 })
