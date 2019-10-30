542d912793656c506116628670633ed13355b6eb
talkmate <talkmate@hongfang-MacBook-Pro.local>
2019-10-30 11:49:02 +0800

    地址

--- config/prod.env.js
+++ config/prod.env.js
@@ -8,7 +8,5 @@ module.exports = {
   REDIRECT_URL: '"http://test-learn.talkmate.com/app/pay-success"',
   APP_KEY: '"talkmateVersion"',
   // APP_SECRET: '"E6DAC3DC3514681FC922ECE1B0CF06EB"' // 测试
-  APP_SECRET: '"5EAE76C6323C85D2E35D4817020D84C9"', // 线上
-  KIDSHAREURL: 'https://mobile-static.talkmate.com/operate/activities/html/miniRecordShare/index.html' // 线上
-  // KIDSHAREURL: 'http://test-learn.talkmate.com:82/miniRecordShare/index.html' // 测试
+  APP_SECRET: '"5EAE76C6323C85D2E35D4817020D84C9"' // 线上
 }
