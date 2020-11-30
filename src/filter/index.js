07204ac5f977130f528157e0aaf0c643486bcb9b
炊军辉 <jack_chui@talkmate.com>
2020-11-30 10:36:58 +0800

    test

--- src/filter/index.js
+++ src/filter/index.js
@@ -2,6 +2,7 @@ export var urlfix = (value, suffix) => {
   if (!value) {
     return ''
   }
+  value = value.replace('http://course-assets.talkmate.com/', 'https://course-assets1.talkmate.com/')
   if (/\?/.test(value)) {
     return value + '&' + suffix
   } else {
